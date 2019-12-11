85f856facbbf20202ca3747b3a7c9c1b86f8edf7
Guest1 <me@email.com>
2019-12-11 11:05:56 -0500

    commit

--- js/shapes.js
+++ js/shapes.js
@@ -29,7 +29,7 @@ window.onload = function() {
 /*
  * Exercise 1.
  */
- // check for debugging 
+ // check for debugging
 
  const sayHello = function() {
    const drawing = document.getElementById('student-canvas-1');
@@ -50,57 +50,50 @@ window.onload = function() {
   * Exercise 2.
   */
 
- const drawRectangle = function() {
+  const drawRectangle = function() {
+    const drawing2 = document.getElementById('student-canvas-2');
+    const output2 = drawing2.getContext('2d');
+    output2.clearRect(0, 0, drawing2.width, drawing2.height);
 
-   const drawing2 = document.getElementById('student-canvas-2');
-   const output2 = drawing2.getContext('2d');
-   output2.clearRect(0, 0, drawing2.width, drawing2.height);
+    var width = 0;
+    var height = 0;
+    var xcoordinate = 0;
+    var ycoordinate = 0;
+    do {
+      var width = window.prompt("Width: ")
+      var height = window.prompt("Height: ")
+      var xcoordinate = window.prompt("X: ")
+      var ycoordinate = window.prompt("Y: ")
+      if (width == null || height == null || xcoordinate == null || ycoordinate == null) {
+        break;
+      }
+      if (width > 1024 || width < 1) {
+        window.alert("Your width must be between 1 and 1024.")
+      }
+      else if (height > 512 || height < 1) {
+        window.alert("Your height must be between 1 and 512.")
+      }
+      else if (xcoordinate < 1 || xcoordinate > 1024) {
+        window.alert("Your x-coordinate must be between 1 and 1024.")
+      }
+      else if (ycoordinate < 1 || ycoordinate > 512) {
+        window.alert("Your y-coordinate must be between 1 and 512.")
+      }
+      else if (isNaN(width) || isNaN(height) || isNaN(xcoordinate) || isNaN(ycoordinate)) {
+        window.alert("One of your values is not a number.")
+      }
+      else if (Number(width) + Number(xcoordinate) > 1024 || Number(height) + Number(ycoordinate) > 512) {
+        window.alert("Your rectangle won't fit on the canvas.")
+      }
+    } while (width > 1024 || width < 1 || height > 512 || height < 1 || xcoordinate < 1 || xcoordinate > 1024 || ycoordinate < 1 || ycoordinate > 512 || isNaN(width) || isNaN(height) || isNaN(xcoordinate) || isNaN(ycoordinate) || Number(width) + Number(xcoordinate) > 1024 || Number(height) + Number(ycoordinate) > 512)
 
-  var width = window.prompt("Width: ")
-  var width = Number(width);
-  var height = window.prompt("Height: ")
-  var height = Number(height);
-  var xcoordinate = window.prompt("X: ")
-  var xcoordinate = Number(xcoordinate);
-  var ycoordinate = window.prompt("Y: ")
-  var ycoordinate = Number(ycoordinate);
-
-    while ((width > 1024) || (width < 1) || (height > 512) || (height < 1) || (xcoordinate > 1024) || (xcoordinate < 1) || (ycoordinate > 512) || (ycoordinate < 1) || (Number.isNaN(width)) || (Number.isNaN(height)) || (Number.isNaN(xcoordinate)) || (Number.isNaN(ycoordinate)) || (xcoordinate + width > 1024) || (ycoordinate + width > 512)) {
-    if (width > 1024 || width < 1) {
-      alert("Your width must be between 1 and 1024.")
+    if (!(width == null) && !(height == null) && !(xcoordinate == null) && !(ycoordinate == null)) {
+      output2.beginPath();
+      output2.rect(xcoordinate, ycoordinate, width, height);
+      output2.closePath();
+      output2.stroke();
     }
-    else if (height > 512 || height < 1) {
-      alert("Your height must be between 1 and 512.")
-    }
-    else if (xcoordinate > 1024 || xcoordinate < 1) {
-      alert("Your x-coordinate must be between 1 and 1024.")
-    }
-    else if (ycoordinate > 512 || ycoordinate < 1) {
-      alert("Your y-coordinate must be between 1 and 512.")
-    }
-    else if (Number.isNaN(width) || Number.isNaN(height) || Number.isNaN(xcoordinate) || Number.isNaN(ycoordinate)) {
-      alert("One of your values is not a number.")
-    }
-    else if (xcoordinate + width > 1024) {
-      alert("Your rectangle won't fit on the canvas.")
-    }
-    else if (ycoordinate + height > 512) {
-      alert("Your rectangle won't fit on the canvas.")
-    }
-    var width = window.prompt("Width: ")
-    var width = Number(width);
-    var height = window.prompt("Height: ")
-    var height = Number(height);
-    var xcoordinate = window.prompt("X: ")
-    var xcoordinate = Number(xcoordinate);
-    var ycoordinate = window.prompt("Y: ")
-    var ycoordinate = Number(ycoordinate);
-}
-
-
-output2.strokeRect(xcoordinate, ycoordinate, width, height);
-}
-
+  };
  /*
   * Exercise 3.
   */
